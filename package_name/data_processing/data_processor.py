7e950e7d79d10be6ee0a4efe3e48975e6fa962e8
Quentin Nicolas <qnicolas@berkeley.edu>
2026-07-13 10:50:50 +0200

    Further main merging (removed the remaining order = 'F')

--- package_name/data_processing/data_processor.py
+++ package_name/data_processing/data_processor.py
@@ -51,7 +51,7 @@ def flatten_input(X: np.ndarray) -> np.ndarray:
     :return:    The flattened, 2-dimensional array.
     """
     nt, ny, nx = X.shape
-    return np.reshape(X, (nt, ny * nx), order="F")
+    return np.reshape(X, (nt, ny * nx))
 
 
 def unflatten_input(X: np.ndarray, ny: int, nx: int) -> np.ndarray:
